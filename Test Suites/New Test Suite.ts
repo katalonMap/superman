5a56a1e94d8e96240c46fc116a011ad795183c2d
chengx <chengx@DESKTOP-NGDSH7O>
2018-05-16 20:48:33 +0800

    修改测试用例

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e41327f4-db3f-4a95-984b-8b0015956b8f</testSuiteGuid>
+   <testCaseLink>
+      <guid>ad132e3e-d0a8-49d6-8075-030404bd1c29</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
