f6f3c13be74cd44dbd0a4f73416216d74840a08d
chengx <chengx@DESKTOP-NGDSH7O>
2018-05-24 09:29:37 +0800

    提交新代码

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7aaf1482-740a-4160-9ef3-a877053c0aca</testSuiteGuid>
+   <testCaseLink>
+      <guid>9a2d66e4-d317-4d24-a9d8-c2df6b411241</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/baidu</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
